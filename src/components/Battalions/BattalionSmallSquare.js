ac740b81502d92a26bb5d1a2f9ec9b50a55be598
Daniel Maile-Wilson <danielmaile@Daniels-MBP.home>
2021-08-03 21:02:00 +0100

    styling
    styling media queries for ipad

--- src/components/Battalions/BattalionSmallSquare.js
+++ src/components/Battalions/BattalionSmallSquare.js
@@ -2,14 +2,12 @@ import classes from "./BattalionSmallSquare.module.css";
 import Card from "../UI/Card";
 const BattalionSmallSquare = (props) => {
   return (
-    <Card>
-      <div
-        className={`${classes.smallSquare} ${
-          props.required ? classes.required : classes.optional
-        }`}
-      >
-        {props.unitType}
-      </div>
+    <Card
+      className={`${classes.smallSquare} ${
+        props.required ? classes.required : classes.optional
+      }`}
+    >
+      {props.unitType}
     </Card>
   );
 };
